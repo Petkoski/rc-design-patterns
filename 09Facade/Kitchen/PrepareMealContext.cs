3fd0b706a84b6d024b8490972379e4e6f0258a92
DESKTOP-PKQP3KC\PETKOSKI <petkoski15@hotmail.com>
2021-09-04 16:22:57 +0200

    Facade example

---
+++ 09Facade/Kitchen/PrepareMealContext.cs
@@ -0,0 +1,40 @@
+﻿//Facade design pattern - taking a system (composition and interaction with many
+//components) and hiding its inner interactions behind simple interface.
+
+using Madness.Dishes;
+using Madness.Recipes;
+
+namespace Madness
+{
+    //I am a facade, you only see the ready meal.
+    //The kitchen is an illusion.
+
+    //Facade - restaurant
+    //Interface that they give you - menu
+    //Whatever happens internally (Chef, ingredients, etc.) is hidden (we are not
+    //concerned with the flexibility of the internal system).
+    public interface IPrepareMealContext
+    {
+        BigMac PrepareBigMac();
+    }
+
+    public class PrepareMealContext
+    {
+        public BigMac PrepareBigMac()
+        {
+            var recipe = new BigMacRecipe();
+
+            //Get beef from fridge
+
+            //Get the tools
+
+            //Fry
+
+            //Etc.
+
+            //Hide composition of components, we just want a specific thing
+
+            return new();
+        }
+    }
+}
